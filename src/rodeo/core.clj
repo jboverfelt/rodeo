eb956bafd31177eaecbd50369b2f41daed15b37c
Justin Overfelt <joverfelt@gmail.com>
2014-01-21 22:11:21 -0500

    added single address capability

--- src/rodeo/core.clj
+++ src/rodeo/core.clj
@@ -7,10 +7,22 @@
 
 (defn batch
   "Takes a vector of addresses and
-  makes a request to Geocodio"
+  makes a request to Geocodio.
+  Returns a Clojure map of results."
   [addresses]
   (let [addr-json (che/generate-string addresses)]
     (che/parse-string 
       (->> {:body addr-json :content-type :json :accept :json}
            (http/post url)
            (:body)) true)))
+
+(defn single
+  "Takes a single address string and
+  makes a request to Geocodio. Returns
+  a Clojure map of results"
+  [address]
+  (let [get-url (str url "&q=" address)]
+    (-> get-url
+        (http/get)
+        (:body)
+        (che/parse-string true))))
