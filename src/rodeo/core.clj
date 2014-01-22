3b49300a57b612c8adaaa767f4c29c203672277c
Justin Overfelt <joverfelt@gmail.com>
2014-01-21 22:19:08 -0500

    adding components function

--- src/rodeo/core.clj
+++ src/rodeo/core.clj
@@ -3,7 +3,15 @@
             [clj-http.client :as http]))
 
 (def ^:const api-key (get (System/getenv) "GEOCODIO_API_KEY"))
-(def ^:const url (str "http://api.geocod.io/v1/geocode?api_key=" api-key))
+(def ^:const geocode-url (str "http://api.geocod.io/v1/geocode?api_key=" api-key))
+(def ^:const parse-url (str "http://api.geocod.io/v1/parse?api_key=" api-key))
+
+(defn- geocodio-get [url address]
+  (let [get-url (str parse-url "&q=" address)]
+    (-> get-url
+        (http/get)
+        (:body)
+        (che/parse-string true)))) 
 
 (defn batch
   "Takes a vector of addresses and
@@ -13,7 +21,7 @@
   (let [addr-json (che/generate-string addresses)]
     (che/parse-string 
       (->> {:body addr-json :content-type :json :accept :json}
-           (http/post url)
+           (http/post geocode-url)
            (:body)) true)))
 
 (defn single
@@ -21,8 +29,12 @@
   makes a request to Geocodio. Returns
   a Clojure map of results"
   [address]
-  (let [get-url (str url "&q=" address)]
-    (-> get-url
-        (http/get)
-        (:body)
-        (che/parse-string true))))
+  (geocodio-get geocode-url address))
+
+(defn components
+  "Takes a single address string and
+  returns a clojure map of the components
+  of that address (street, city, etc as a 
+  Clojure map"
+  [address]
+  (geocodio-get parse-url address)) 
