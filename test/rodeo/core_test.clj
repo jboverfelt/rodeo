52ff190e59e65c4831511186720bd3d13c4c1dbf
Justin Overfelt <joverfelt@gmail.com>
2014-01-24 20:48:37 -0500

    adding basic tests

--- test/rodeo/core_test.clj
+++ test/rodeo/core_test.clj
@@ -1,7 +1,50 @@
 (ns rodeo.core-test
   (:require [clojure.test :refer :all]
-            [rodeo.core :refer :all]))
+            [rodeo.core :as rodeo]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def api-key (get (System/getenv) "GEOCODIO_API_KEY"))
+(def batch-data ["1 Infinite Loop Cupertino CA 95014" 
+                 "54 West Colorado Boulevard Pasadena CA 91105"
+                 "826 Howard Street San Francisco CA 94103"])  
+(def single-data (first batch-data))
+
+(defn batch-results? [resp]
+  (-> resp
+      (:results)
+      (first)
+      (:response)
+      (:results)
+      (vector?)))
+
+(defn single-result? [resp]
+  (-> resp
+      (:results)
+      (vector?)))
+
+(defn component-result? [resp]
+  (-> resp
+      (:address_components)
+      (:street)
+      (string?)))
+
+(deftest batch
+  (testing "batch geocode calls"
+    (is (batch-results? (rodeo/batch batch-data)))
+    (is (batch-results? (rodeo/batch batch-data api-key))))
+  (testing "bad api key"
+    (is (thrown-with-msg? Exception #"status 403" (rodeo/batch batch-data "bogus key")))))
+
+(deftest single
+  (testing "single geocode call"
+    (is (single-result? (rodeo/single single-data)))
+    (is (single-result? (rodeo/single single-data api-key))))
+  (testing "bad api key"
+    (is (thrown-with-msg? Exception #"status 403" (rodeo/single single-data "bogus key")))))
+
+(deftest components
+  (testing "address component parsing"
+    (is (component-result? (rodeo/components single-data)))
+    (is (component-result? (rodeo/components single-data api-key))))
+  (testing "bad api key"
+    (is (thrown-with-msg? Exception #"status 403" (rodeo/components single-data "bogus key")))))
+  
