79f7cdf9a23af1f7dd662964b6e68fb237b8e4ba
Justin Overfelt <joverfelt@gmail.com>
2014-03-02 21:12:26 -0500

    adding coveralls support

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/jboverfelt/rodeo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cloverage "1.0.2"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.3.1"]
                  [clj-http-lite "0.2.0"]])
