8c75b5f98d4a79c704002e86a22bd74dfe453610
Justin Overfelt <joverfelt@gmail.com>
2014-03-25 21:14:01 -0400

    updating clojure version to 1.6.0

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cloverage "1.0.2"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
                  [clj-http-lite "0.2.0"]])
